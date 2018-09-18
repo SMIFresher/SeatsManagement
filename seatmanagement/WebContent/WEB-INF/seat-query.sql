4875a1c02ec9dac106b63c1bce8e8be5114e8276
Aathitya Prabu <aathtiyaprabu.a@mookambikainfo.com>
2018-09-18 17:45:12 +0530

    Updated query is committed

---
+++ seatmanagement/WebContent/WEB-INF/seat-query.sql
@@ -0,0 +1,141 @@
+drop database seat;
+
+create database seat;
+
+use seat;
+
+CREATE TABLE `seat`.`building` ( 
+ `building_id` VARCHAR(36) NOT NULL,  
+ `building_name` VARCHAR(45) NOT NULL, 
+ `building_address` VARCHAR(255) NOT NULL, 
+ `building_location` VARCHAR(45) NOT NULL,
+ PRIMARY KEY (`building_id`));
+ 
+CREATE TABLE `seat`.`floor` (  `floor_id` VARCHAR(36) NOT NULL,
+  `building_id` VARCHAR(36) NOT NULL,  
+  `floor_type` VARCHAR(45) NOT NULL,  
+  PRIMARY KEY (`floor_id`),  
+  INDEX `building_id_idx` (`building_id` ASC), 
+  CONSTRAINT `building_id`    FOREIGN KEY (`building_id`)   
+  REFERENCES `seat`.`building` (`building_id`)   
+  ON DELETE NO ACTION    ON UPDATE NO ACTION);
+  
+CREATE TABLE `seat`.`block` ( 
+ `block_id` VARCHAR(36) NOT NULL, 
+ `block_type` VARCHAR(45) NOT NULL,  
+ `block_capacity` VARCHAR(45) NOT NULL,  
+ `block_description` VARCHAR(255) NOT NULL,  
+ `block_measurement` DOUBLE NOT NULL,  
+ `floor_id` VARCHAR(36) NOT NULL, 
+ PRIMARY KEY (`block_id`), 
+ INDEX `floor_id_idx` (`floor_id` ASC), 
+ CONSTRAINT `floor_id`   
+ FOREIGN KEY (`floor_id`)    
+ REFERENCES `seat`.`floor` (`floor_id`)  
+ ON DELETE NO ACTION    ON UPDATE NO ACTION);
+ 
+CREATE TABLE `seat`.`block_specification` 
+(  `specification_id` VARCHAR(36) NOT NULL,  
+`block_id` VARCHAR(36) NOT NULL, 
+ `airconditioning` TINYINT NULL,  
+ `network_type` TINYINT NULL,  
+ `television` TINYINT NULL,  
+ `projector` TINYINT NULL,
+ `board` TINYINT NULL, 
+ `chair` TINYINT NULL,  
+ `mike` TINYINT NULL, 
+ PRIMARY KEY (`specification_id`),  
+ INDEX `block_id_idx` (`block_id` ASC),  
+ CONSTRAINT `block_id`    FOREIGN KEY (`block_id`)    
+ REFERENCES `seat`.`block` (`block_id`)   
+ ON DELETE NO ACTION    ON UPDATE NO ACTION);
+ 
+CREATE TABLE seat.team (   
+ team_id varchar(36) NOT NULL,  
+ team_name varchar(100) NOT NULL,   
+ team_head varchar(50) NOT NULL,    
+ team_members_count int,    
+ PRIMARY KEY (team_id))ENGINE=InnoDB;
+ 
+CREATE TABLE seat.employee (
+	employee_id varchar(36) NOT NULL,
+    employee_roll varchar(50) NOT NULL,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    designation varchar(50) NOT NULL,
+    date_of_joining DATE NOT NULL,
+    team_id varchar(36) NOT NULL,
+    PRIMARY KEY (employee_id),
+    KEY `employee_fk_1` (`team_id`),
+    CONSTRAINT `employee_fk_1` FOREIGN KEY (`team_id`) 
+ 	REFERENCES `team` (`team_id`)
+)ENGINE=InnoDB;
+
+CREATE TABLE seat.system (
+    system_id varchar(36) NOT NULL,
+	employee_id varchar(36),
+	system_name varchar(50) NOT NULL,
+    system_type varchar(50) NOT NULL,
+    network_type varchar(50),
+    allotment_status varchar(50) NOT NULL,
+    PRIMARY KEY (system_id),
+	KEY `system_fk_1` (`employee_id`),
+    CONSTRAINT `system_fk_1` FOREIGN KEY (`employee_id`) 
+ 	REFERENCES `employee` (`employee_id`)
+)ENGINE=InnoDB;
+
+CREATE TABLE seat.reallocation (
+    reallocation_id varchar(36) NOT NULL,
+	employee_id varchar(36) NOT NULL,
+	block_id varchar(36) NOT NULL,
+    reallocated_seating_detail_id varchar(36),
+    reallocation_date date,
+    reallocation_status varchar(50) NOT NULL,
+	alloted_by varchar(50) NOT NULL,
+    PRIMARY KEY (reallocation_id),
+	KEY `reallocation_fk_1` (`employee_id`),
+    CONSTRAINT `reallocation_fk_1` FOREIGN KEY (`employee_id`) 
+ 	REFERENCES `employee` (`employee_id`),
+	KEY `reallocation_fk_2` (`block_id`),
+    CONSTRAINT `reallocation_fk_2` FOREIGN KEY (`block_id`) 
+ 	REFERENCES `block` (`block_id`)
+)ENGINE=InnoDB;
+
+CREATE TABLE `seat`.`seating` (  
+`seating_id` VARCHAR(36) NOT NULL, 
+ `block_id` VARCHAR(36) NOT NULL,  
+ `seat_occupied` INT NOT NULL,  
+ `team_id` VARCHAR(36) NOT NULL,  
+ PRIMARY KEY (`seating_id`),  
+ INDEX `block_id_idx` (`block_id` ASC), 
+ INDEX `team_id_idx` (`team_id` ASC), 
+ CONSTRAINT `block_id1`     FOREIGN KEY (`block_id`)
+ REFERENCES `seat`.`block` (`block_id`)    
+ ON DELETE NO ACTION    ON UPDATE NO ACTION, 
+ CONSTRAINT `team_id`     FOREIGN KEY (`team_id`)   
+ REFERENCES `seat`.`team` (`team_id`)     ON DELETE NO ACTION    ON UPDATE NO ACTION 
+ );
+ 
+CREATE TABLE seat.seating_detail (
+    seating_detail_id varchar(36) NOT NULL,
+	seating_id varchar(36) NOT NULL,
+	system_id varchar(36)  NOT NULL,
+    seating_date date,
+    PRIMARY KEY (seating_detail_id),
+	KEY `seating_detail_fk_1` (`seating_id`),
+    CONSTRAINT `seating_detail_fk_1` FOREIGN KEY (`seating_id`) 
+ 	REFERENCES `seating` (`seating_id`),
+	KEY `seating_detail_fk_2` (`system_id`),
+    CONSTRAINT `seating_detail_fk_2` FOREIGN KEY (system_id) 
+ 	REFERENCES `system` (`system_id`)
+)ENGINE=InnoDB;
+
+CREATE TABLE seat.additional_device (
+    additional_device_id varchar(36) NOT NULL,
+	device_name varchar(100) NOT NULL,
+	system_id varchar(36),
+    PRIMARY KEY (additional_device_id),
+	KEY `additional_device_fk_1` (`system_id`),
+    CONSTRAINT `additional_device_fk_1` FOREIGN KEY (`system_id`) 
+ 	REFERENCES `system` (`system_id`)
+)ENGINE=InnoDB;
