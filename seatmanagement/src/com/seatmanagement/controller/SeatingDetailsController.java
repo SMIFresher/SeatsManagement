6fbb36d3ef5cf03a39635d75b79cd53f06acb9a3
Aathitya Prabu <aathtiyaprabu.a@mookambikainfo.com>
2018-09-19 21:16:58 +0530

    Code committed for get service for System Details

--- seatmanagement/src/com/seatmanagement/controller/SeatingDetailsController.java
+++ seatmanagement/src/com/seatmanagement/controller/SeatingDetailsController.java
@@ -3,28 +3,33 @@ package com.seatmanagement.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.gson.Gson;
 import com.seatmanagement.model.SeatingDetails;
 import com.seatmanagement.service.SeatingDetailsService;
 
-@Controller
+@RestController
 @RequestMapping("/seatingdetails")
 public class SeatingDetailsController {
 
 	@Autowired
 	SeatingDetailsService seatingDetailsService;
 	
-	@RequestMapping(value="/getAll",method=RequestMethod.GET ,produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<SeatingDetails>> getAllSeatingDetails(){
-		//List<SeatingDetails> seatingDetails = seatingDetailsService.getAllSeatingDetails();
-		return new ResponseEntity<>(seatingDetailsService.getAllSeatingDetails(), HttpStatus.OK);
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value="/getAll",method=RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity getAllSeatingDetails(){
+		List<Object> objectList = seatingDetailsService.getAllSeatingDetails();
+		
+		
+		String string=new Gson().toJson(objectList);
+		return ResponseEntity.ok(string);
 	}
 	
 	@RequestMapping(value="/save",method=RequestMethod.POST ,produces = MediaType.APPLICATION_JSON_VALUE)
