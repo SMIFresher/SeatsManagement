5c0cc973399fdc818200eb2017f3411281717cdc
prithiviraj <prithiviraj.m@mookambikainfo.com>
2018-09-20 12:48:42 +0530

    renamed genericDao

---
+++ seatmanagement/src/com/seatmanagement/dao/GenericDao.java
@@ -0,0 +1,14 @@
+package com.seatmanagement.dao;
+
+import java.util.List;
+
+public interface GenericDao<T> {
+
+	public void insert(T t);
+	public T getById(T t,int id);
+	public List getAll(T t);
+	public boolean delete(T t);
+	public boolean update(T t);
+	
+}
+ 
