081a4a3d40bdf941299f68013d81ec25bfed37f2
Vijayakumar <vijayakumar.s@mookambikainfo.com>
2018-10-08 20:04:41 +0530

    Autowiretest model

---
+++ seatmanagement/src/com/seatmanagement/model/AutowireTestModel.java
@@ -0,0 +1,15 @@
+package com.seatmanagement.model;
+
+public class AutowireTestModel {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
