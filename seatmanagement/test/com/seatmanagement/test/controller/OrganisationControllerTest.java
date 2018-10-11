ce2a6bd3e3fd2fabcd95bbda30bf24e69722a6d9
Vijayakumar <vijayakumar.s@mookambikainfo.com>
2018-10-11 21:29:22 +0530

    Junit Integration

--- seatmanagement/test/com/seatmanagement/test/controller/OrganisationControllerTest.java
+++ seatmanagement/test/com/seatmanagement/test/controller/OrganisationControllerTest.java
@@ -2,10 +2,9 @@ package com.seatmanagement.test.controller;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,20 +16,18 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import com.seatmanagement.dao.GenericDao;
-import com.seatmanagement.model.Organisation;
-import com.seatmanagement.service.OrganisationService;
+import com.google.gson.Gson;
 
 @RunWith(JUnitPlatform.class)
-public class OrganisationControllerTest {
+public class OrganisationControllerTest  {
+	
+	private static final Logger logger = LoggerFactory.getLogger(OrganisationControllerTest.class);
 	
 	private static final String HOST = "localhost";
 	private static final String PORT = "8080";
@@ -40,15 +37,6 @@ public class OrganisationControllerTest {
 	private static final String REQUEST_TYPE = "requestType";
 	private static final String REQUEST_TYPE_AJAX = "AJAX";
 
-	private static final Logger logger = LoggerFactory.getLogger(OrganisationControllerTest.class);
-	
-	@Autowired
-	private GenericDao genericDaoMock;
-	
-	@Autowired
-	private OrganisationService organisationService;
-
-	@Disabled
 	@Test
 	public void saveOrganisationTest() {
 		try {
@@ -75,7 +63,6 @@ public class OrganisationControllerTest {
 		}
 	}
 
-	@Disabled
 	@Test
 	public void saveOrganisationNotEmptyValidationTest() {
 		try {
@@ -104,7 +91,6 @@ public class OrganisationControllerTest {
 		}
 	}
 	
-	@Disabled
 	@Test
 	public void saveOrganisationWithoutRequestParamTest() {
 		try {
@@ -131,7 +117,6 @@ public class OrganisationControllerTest {
 		}
 	}
 
-	@Disabled
 	@Test
 	public void getOrganisationViewTest() {
 		try {
@@ -152,34 +137,6 @@ public class OrganisationControllerTest {
 		}
 	}
 
-	/*@Test
-	public void getOrganisationByIdTest() {
-		try {
-			// insert test record
-			String organisationName = "getOrganisationByIdTest";
-			Organisation organisation = saveOrganisation(organisationName);
-
-			// Overall flow test
-			MvcResult result = mockMvc
-					.perform(post("/organisation/getOrganisationById").param("organisationId",
-							organisation.getOrganisationId().toString()))
-					.andDo(print()).andExpect(status().isOk()).andReturn();
-
-			// assert record in response
-			String content = result.getResponse().getContentAsString();
-			Gson gson = new Gson();
-			Organisation organisationFromDB = gson.fromJson(content, Organisation.class);
-			Assert.assertNotNull(organisationFromDB);
-
-			// delete test record
-			deleteOrganisationById(organisation.getOrganisationId());
-		} catch (Exception e) {
-			fail(e.getMessage());
-
-			e.printStackTrace();
-		}
-	}*/
-
 	@Test
 	public void getAllOrganisationsNormalFlowTest() {
 		try {
@@ -188,11 +145,33 @@ public class OrganisationControllerTest {
 		    HttpPost httpPost = new HttpPost(BASE_URL+"/getAllOrganisations");
 		    httpPost.setHeader(REQUEST_TYPE, REQUEST_TYPE_AJAX);
 		    
-		    List organisations = new ArrayList();
-		    organisations.add(new Organisation());
-		    organisations.add(new Organisation());
+		    CloseableHttpResponse response = client.execute(httpPost);
 		    
-		    when(genericDaoMock.getAll(any((Organisation.class)))).thenReturn(organisations);
+		    String responseBody = EntityUtils.toString(response.getEntity());
+		    List organisationList = new Gson().fromJson(responseBody, List.class);
+		    
+		    assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
+		    assertThat(organisationList.size(), is(2));
+		    
+		    client.close();
+		} catch (Exception e) {
+			fail(e.getMessage());
+
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void deleteOrganisationByIdNormalFlowTest() {
+		try {
+			CloseableHttpClient client = HttpClients.createDefault();
+			
+		    HttpPost httpPost = new HttpPost(BASE_URL+"/deleteOrganisationById");
+		    httpPost.setHeader(REQUEST_TYPE, REQUEST_TYPE_AJAX);
+		    
+		    List<BasicNameValuePair> params = new ArrayList<BasicNameValuePair>();
+		    params.add(new BasicNameValuePair("organisationId", "3150e520-cd6b-11e8-b568-0800200c9a66"));
+		    httpPost.setEntity(new UrlEncodedFormEntity(params));
 		    
 		    CloseableHttpResponse response = client.execute(httpPost);
 		    
@@ -207,52 +186,29 @@ public class OrganisationControllerTest {
 			e.printStackTrace();
 		}
 	}
-
-	/*@Test
-	public void deleteOrganisationByIdTest() {
+	
+	@Test
+	public void deleteOrganisationByIdRequestParamNullTest() {
 		try {
-			// insert test record
-			Organisation organisation = saveOrganisation("Test Organisation Delete");
-
-			// Overall flow test
-			mockMvc.perform(post("/organisation/deleteOrganisationById").param("organisationId",
-					organisation.getOrganisationId().toString())).andDo(print()).andExpect(status().isOk());
-
-			// assert record deleted in db
-			Object deleteScenarioOrganisation = genericDao.getById(organisation, organisation.getOrganisationId());
-
-			Assert.assertNull(deleteScenarioOrganisation);
+			CloseableHttpClient client = HttpClients.createDefault();
+			
+		    HttpPost httpPost = new HttpPost(BASE_URL+"/deleteOrganisationById");
+		    httpPost.setHeader(REQUEST_TYPE, REQUEST_TYPE_AJAX);
+		    
+		    CloseableHttpResponse response = client.execute(httpPost);
+		    
+		    String responseBody = EntityUtils.toString(response.getEntity());
+		    
+		    assertThat(response.getStatusLine().getStatusCode(), equalTo(400));
+		    assertThat(responseBody, containsString("\"ERROR_MESSAGE\":\"Internal Server Error\""));
+		    assertThat(responseBody, containsString("\"ERROR_CODE\":9002"));
+		    
+		    client.close();
 		} catch (Exception e) {
 			fail(e.getMessage());
 
 			e.printStackTrace();
 		}
 	}
-
-	private Organisation saveOrganisation(String organisationName) throws Exception {
-		Organisation organisation = new Organisation();
-		organisation.setOrganisationName(organisationName);
-
-		organisation = (Organisation) genericDao.saveOrUpdate(organisation);
-
-		return organisation;
-	}
-
-	private Organisation getOrganisationByName(String organisationName) throws Exception {
-		Organisation organisation = null;
-
-		List<Organisation> testOrganisations = organisationDao.getOrganisationByName(organisationName);
-		if (Objects.nonNull(testOrganisations) && !testOrganisations.isEmpty()) {
-			organisation = testOrganisations.get(0);
-		}
-
-		return organisation;
-	}
-
-	private void deleteOrganisationById(UUID organisationId) throws Exception {
-		Organisation organisation = new Organisation();
-		organisation.setOrganisationId(organisationId);
-		genericDao.delete(organisation);
-	}*/
 
 }
